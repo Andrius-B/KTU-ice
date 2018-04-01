ce6ccea68aaae4a35cde99b8843c54c549e4ce60
Andrius-B <drugelissp@gmail.com>
2018-04-01 11:10:47 +0300

    Added week view library sources to the project

---
+++ week-view-lib/src/main/java/com/alamkanak/weekview/DateTimeInterpreter.java
@@ -0,0 +1,12 @@
+package com.alamkanak.weekview;
+
+import java.util.Calendar;
+
+/**
+ * Created by Raquib on 1/6/2015.
+ */
+public interface DateTimeInterpreter {
+    public String interpretDate(Calendar date);
+
+    public String interpretTime(int hour, int minutes);
+}
